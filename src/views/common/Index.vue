de1278d0d196d61054af00ff23b01f278dfb8c45
Frank Xie <fr6nkxie@gmail.com>
2021-06-23 14:08:09 +0800

    elevator

--- src/views/common/Index.vue
+++ src/views/common/Index.vue
@@ -40,8 +40,11 @@ export default {
   width: 100%;
 }
 .elevator {
-  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 3px 5px 0 rgba(0, 0, 0, 0.14),
-    0 1px 9px 0 rgba(0, 0, 0, 0.12) !important;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14),
+    0px 1px 5px 0px rgba(0, 0, 0, 0.12) !important;
+  &.radius {
+    border-radius: 8px;
+  }
 }
 .fs-18 {
   font-size: 18px;
